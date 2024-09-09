b0e54f9a13838f5610396adfeb01bb1d0bd73d06
Sakshi <gangwani.sakshi15@gmail.com>
2024-09-09 19:04:37 +0530

    🚀 added generative AI function

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import './Main.css';
 import {assets} from '../../assests/assets';
+import { Context } from "../../context/Context";
 
 function Main(){
+    const {onSent, recentPrompt, showResult, loading, resultData, setInput, input} = useContext(Context);
     return (
         <div className="main">
             <div className="nav">
@@ -10,7 +12,7 @@ function Main(){
                 <img src={assets.user_icon} alt=""/>
             </div>
             <div className="main-container">
-                <div className="greet">
+                {!showResult?<><div className="greet">
                     <p><span>Hello, Sakshi</span></p>
                     <p>How can I help you today?</p>
                 </div>
@@ -31,14 +33,24 @@ function Main(){
                         <p>Improve the readability of the following code.</p>
                         <img src={assets.code_icon} alt=""/>
                     </div>
-                </div>
+                </div></> : <div className="result">
+                    <div className="result-title">
+                        <img src={assets.user_icon} alt=""/>
+                        <p>{recentPrompt}</p>
+                    </div>
+                    <div className="result-data">
+                        <img src={assets.gemini_icon} alt=""/>
+                        {loading ? <div className="loader"><hr/><hr/><hr/></div> : <p dangerouslySetInnerHTML={{__html:resultData}}></p>}
+                    </div>
+                </div>}
+                
                 <div className="main-bottom">
                     <div className="search-box">
-                        <input type="text" placeholder="Enter a prompt here"/>
+                        <input type="text" onChange={(e) => setInput(e.target.value)} value={input} placeholder="Enter a prompt here"/>
                         <div>
                             <img src={assets.gallery_icon} alt=""/>
                             <img src={assets.mic_icon} alt=""/>
-                            <img src={assets.send_icon} alt=""/>
+                            <img onClick={()=>onSent()} src={assets.send_icon} alt=""/>
                         </div>
                     </div>
                     <p className="bottom-info">Gemini may display inaccurate info, including about people, so double-check its responses. Your privacy and Gimini Apps.</p>
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,47 +1,63 @@
-import React, { useState } from "react";
-import ReactDOM from 'react-dom/client';
-import './Sidebar.css';
-import {assets} from '../../assests/assets';
+import React, { useContext, useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./Sidebar.css";
+import { assets } from "../../assests/assets";
+import { Context } from "../../context/Context";
 
 function Sidebar() {
-    const [extended, setExtended] = useState(false);
+  const [extended, setExtended] = useState(false);
+  const { onSent, previousPrompt, setRecentPrompt, newChat } = useContext(Context);
 
-    function showHide() {
-        setExtended(!extended);
-    }
-    return (
-        <div className="sidebar">
-            <div className="top">
-                <img onClick={showHide} className="menu" src={assets.menu_icon} alt=""/>
-                <div className="new-chat">
-                    <img src={assets.plus_icon} alt=""/>
-                    {extended ? <p>New Chat</p> : null}
-                </div>
-                {extended ? <div className="recent">
-                    <p className="recent-title">Recent</p>
-                    <div className="recent-entry">
-                        <img src={assets.message_icon} alt=""/>
-                        <p>What is react ...</p>
-                    </div>
-                </div> : null}
-                
-            </div>
-            <div className="bottom">
-                <div className="bottom-item recent-entry">
-                    <img src={assets.question_icon} alt=""/>
-                    {extended ? <p>Help</p> : null}
-                </div>
-                <div className="bottom-item recent-entry">
-                    <img src={assets.history_icon} alt=""/>
-                    {extended ? <p>Activity</p> : null}
-                </div>
-                <div className="bottom-item recent-entry">
-                    <img src={assets.setting_icon} alt=""/>
-                    {extended ? <p>Settings</p> : null}
-                </div>
-            </div>
+  const loadPrompt = async(prompt) => {
+    setRecentPrompt(prompt)
+    onSent(prompt)
+  }
+  function showHide() {
+    setExtended(!extended);
+  }
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <img
+          onClick={showHide}
+          className="menu"
+          src={assets.menu_icon}
+          alt=""
+        />
+        <div onClick={()=>newChat()} className="new-chat">
+          <img src={assets.plus_icon} alt="" />
+          {extended ? <p>New Chat</p> : null}
         </div>
-    );
+        {extended ? (
+          <div className="recent">
+            <p className="recent-title">Recent</p>
+            {previousPrompt.map((item, index) => {
+              return (
+                <div onClick={()=>loadPrompt(item)} className="recent-entry">
+                  <img src={assets.message_icon} alt="" />
+                  <p>{item.slice(0,18)} ...</p>
+                </div>
+              );
+            })}
+          </div>
+        ) : null}
+      </div>
+      <div className="bottom">
+        <div className="bottom-item recent-entry">
+          <img src={assets.question_icon} alt="" />
+          {extended ? <p>Help</p> : null}
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.history_icon} alt="" />
+          {extended ? <p>Activity</p> : null}
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.setting_icon} alt="" />
+          {extended ? <p>Settings</p> : null}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
